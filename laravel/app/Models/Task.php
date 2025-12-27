b04d310b4f1f2a9c1672d9f9043eee0c2800ca31
CHHEANG HOKLENG I3GIC-A <propronplanetearth@gmail.com>
2025-12-27 08:59:00 +0700

    laravel-commitTP06-task1

---
+++ laravel/app/Models/Task.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = ['title', 'description', 'project_id', 'assigned_to', 'status'];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function assignedUser()
+    {
+        return $this->belongsTo(User::class, 'assigned_to');
+    }
+}
---
+++ laravel/database/migrations/Project.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace database\migrations;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Project extends Model
+{
+    //
+}
